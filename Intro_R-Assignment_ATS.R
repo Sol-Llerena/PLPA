0af4eba8098b2338560a83016d677abdbf2e3dac
Alex Swystun <122708137+Aswystun@users.noreply.github.com>
2025-02-06 14:12:54 -0600

    updating

---
+++ Intro_R-Assignment_ATS.R
@@ -0,0 +1,18 @@
+#5 pts. Explain the following things about R and Rstudio:
+#o Where do you type code?
+##You type code into the source
+#  o Where is the output of the code?
+## The code outputs into the Console.
+#  o Where do plots show up?
+## The plots show up in the bottom right hand corner in the plots tab.
+#  o What is a global environment?
+## 
+#  o How do you get help for a function through R studio?
+##
+#  o What is an R package?
+##
+#  o What is a function?
+#  o How do you find the installed and loaded packages?
+#  o What is a working directory, and how do you find it?
+#  o What is a relative file path and how is it different than an absolute file path?
+getwd()
---
+++ coding_notes_ATS.R
@@ -0,0 +1,1 @@
+# I have had experience with R in the past, and I will be starting at functions.
